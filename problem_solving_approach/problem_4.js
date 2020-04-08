8bdff937bc683167b31232a2984545150c2d13b7
Tzvi-H <16guitar@gmail.com>
2020-04-08 15:37:03 -0500

    problem_4 iteration 2

--- problem_solving_approach/problem_4.js
+++ problem_solving_approach/problem_4.js
@@ -17,33 +17,42 @@ Questions
 
 /*
 0. create a result array
-1. seperate all numbers
-2. for each Number(number)
-  2a. if array is empty 
-    2aa. push the number to the array         
-  2b. else 
-    2ba. get the last number in the results
-    2bb. generate the next largerNum with ending digit (SUBPROCESS)
-    2bc. push the generatedNumber into the results array
+1. split all numberStrings
+2. map into array of numbers
+3. for each number and index
+  3a. if index is 0
+    3aa. push the number to the results         
+  3b. else 
+    3ba. get the last number in the results
+    3bb. generate the next largerNum with ending digit of number (SUBPROCESS)
+    3bc. push the generatedNumber into the results array
+4. return results
 
 SUBPROCESS
-0. Given a currentNum and a digit (7, 2) (14, 1)
+0. Given a currentNum and a digit
 1. increment currentNum until it's last digit matches the digit 
 2. return currentNum  
 */
 
 function extractNumbers(stringNums) {
   let results = [];
-  let numbers = stringNums.split(', ').map(char => Number(char)); // [1, 3, 7, 2, 4, 1]
-  numbers.forEach((digit, index) => {
-    if (index === 0) {
-      results.push(digit);
+  let numbers = stringNums.split(', ');
+  numbers.forEach((value, index) => {
+    let currentNum = results[results.length - 1];
+    if (!/-|:|\.{2}/.test(value)) {
+      let digit = Number(value);
+      if (!currentNum) {
+        results.push(digit);
+      } else {
+        results.push(nextNumber(currentNum, digit));
+      }
     } else {
-      let currentNum = results[index - 1];
-      results.push(nextNumber(currentNum, digit));
-    }
+      let nums = value.split(/[-:.]/).map(char => Number(char));
+      //let range = 
+
+    }  
   })
-  console.log(results);
+  return results;
 }
 
 function nextNumber(currentNum, digit) {
@@ -53,9 +62,10 @@ function nextNumber(currentNum, digit) {
   return currentNum;
 }
 
-extractNumbers("1, 3, 7, 2, 4, 1")  // [1, 3, 7, 12, 14, 21]
-// extractNumbers("1-3, 1-2") // [1, 2, 3, 11, 12]
-// extractNumbers("1:5:2") // [1, 2, 3, 4, 5, 6, ... 12]
-// extractNumbers("104-2") // [104, 105, ... 112]
-// extractNumbers("104-02") // [104, 105, ... 202]
-// extractNumbers("545, 64:11") // [545, 564, 565, .. 611]
+console.log(extractNumbers("1, 3, 7, 2, 4, 1"));  // [1, 3, 7, 12, 14, 21]
+// console.log(extractNumbers("1-3, 1-2")); // [1, 2, 3, 11, 12]
+// console.log(extractNumbers("1:5:2")); // [1, 2, 3, 4, 5, 6, ... 12]
+// console.log(extractNumbers("104-2")); // [104, 105, ... 112]
+// console.log(extractNumbers("104-02")); // [104, 105, ... 202]
+// console.log(extractNumbers("545, 64:11")); // [545, 564, 565, .. 611]
+
