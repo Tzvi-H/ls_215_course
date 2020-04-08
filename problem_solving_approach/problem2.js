9c5c5147583691c669a784cbfa65b0f073b689a5
Tzvi-H <16guitar@gmail.com>
2020-04-08 10:54:50 -0500

    updated pseudocode

--- problem_solving_approach/problem2.js
+++ problem_solving_approach/problem2.js
@@ -15,19 +15,19 @@ Examples
   - "8763" => 7733 => 20 => valid
   - "2323 2005 7766 3554" => valid
 Data Structure
-  - string
+  - array
 Algorithm
-  - initialize a needsDoubling variable to false
-  - initialize a sum to 0
   - replace all non digit characters with blanks  
-  - loop from back of string, for each index
-    - intialize a num variable to the current element coerced into a number
-    - if needsDoubling is true
+  - split the chars into an array
+  - reverse the array
+  - map the characters into numbers
+  - reduce the array starting with a 0 accumulator, for each element and index
+    - if index is odd
       - double the number
         - if doubling results in greater then 9
           - subtract 9
-    - add the num to the sum
-  - return whether the last digit in sum is 0       
+    - return the accumulator + num
+  - return whether the last digit in the reduced value is 0       
 Questions
 */
 
