43ee0d95a802fdc32a237ed8695f9cf16e799976
Tzvi-H <16guitar@gmail.com>
2020-04-09 14:40:07 -0500

    problem-4 solution

--- problem_solving_approach/problem_4.js
+++ problem_solving_approach/problem_4.js
@@ -1,85 +1,70 @@
-function splitIntoNums(string) {
+function shorthandNumbers(string) {
+  return numStringToArray(string).reduce(function(nums, numString) {
+    return nums.concat(generateNums(nums, numString))
+  }, []);
+}
+
+function generateNums(array, numString) {
+  let currentNum = array[array.length - 1];
+  if (isRange(numString)) {
+    return generateRange(currentNum, numString);
+  } else {
+    return generateNum(currentNum, numString);
+  }
+}
+
+function numStringToArray(string) {
   return string.split(', ');
 }
 
-function includesRange(numString) {
+function isRange(numString) {
   return /-|:|\.{2}/.test(numString);
 }
 
-function isEmpty(array) {
-  return array.length === 0;
-}
-
-function lastElement(array) {
-  return array[array.length - 1];
-}
-
-function nextValue(num, endingString) {
-  while (!endsWith(num, endingString)) {
+function nextNum(num, endingString) {
+  while (!String(num).endsWith(endingString)) {
     num += 1;
   }
   return num;
 }
 
-function endsWith(num, endingString) {
-  return String(num).endsWith(endingString);
-}
-
-function splitIntoRange(string) {
-  return string.split(/-|:|\.{2}/);
-}
-
-function incrementNumStringByOne(numString) {
-  let nextNum = Number(numString) + 1;
-  if (nextNum === 100) {
-    nextNum = 0;
+function generateNum(currentNum, numString) {
+  if (currentNum === undefined) {
+    return Number(numString);
+  } else {
+    return nextNum(currentNum, numString);
   }
-  return String(nextNum);
 }
 
-function convertShortHandNums(stringOfNums) {
-  let results = [];
-  let stringNums = splitIntoNums(stringOfNums);
-  stringNums.forEach(numString => {
-    if (includesRange(numString)) {
-      let range = splitIntoRange(numString);
-      for (let index = 0; index < range.length - 1; index += 1) {
-        let start = range[index];
-        let end = range[index + 1];
-        if (isEmpty(results)) {
-          results.push(Number(start));
-          start = incrementNumStringByOne(start);
-        }
-        let lastNum = lastElement(results);
-        if (endsWith(lastNum, start)) {
-          start = incrementNumStringByOne(start);
-        }
-        while (true) {
-          let num = nextValue(lastNum, start);
-          results.push(num);
-          lastNum = lastElement(results);
-          if (endsWith(lastNum, end)) {
-            break;
-          }
-          start = incrementNumStringByOne(start);
-        }
+function generateRange(currentNum, rangeString) {
+  let range = [];
+  let rangeStrings = splitRange(rangeString);
+  rangeStrings.slice(0, -1).forEach(function(_, idx, arr) {
+    currentNum = currentNum || Number(rangeStrings[0]);
+    let min = nextNum(currentNum, rangeStrings[idx]);
+    if (min === Number(rangeStrings[idx]) && idx > 0) {
+      min += 1;
+    }
+    let max = rangeStrings[idx + 1];
+    while (true) {
+      range.push(min);
+      if (String(min).endsWith(max)) {
+        break;
       }
-    } else {
-      if (isEmpty(results)) {
-        results.push(Number(numString));
-      } else {
-        let lastNum = lastElement(results);
-        let num = nextValue(lastNum, numString);
-        results.push(num);
-      }
+      min = min + 1;
     }
   })
-  return results;
+  return range;
 }
 
-console.log(convertShortHandNums('1, 3, 7, 2, 4, 1')); // 1, 3, 7, 12, 14, 21
-console.log(convertShortHandNums('1-3, 1-2'));         // 1, 2, 3, 11, 12
-console.log(convertShortHandNums('104-2'));            // 104, 105, ... 112
-console.log(convertShortHandNums('104-02'));           // 104, 105, ... 202
-console.log(convertShortHandNums('545, 64:11'));       // 545, 564, 565, .. 611
-console.log(convertShortHandNums('1:5:2'));            // 1, 2, 3, 4, 5, 6, ... 12
+function splitRange(rangeString) {
+  return rangeString.split(/-|:|\.{2}/);
+}
+
+
+// console.log(shorthandNumbers('1, 3, 7, 2, 4, 1')); // 1, 3, 7, 12, 14, 21
+console.log(shorthandNumbers('1-3, 1-2'));
+console.log(shorthandNumbers('104-2'));
+console.log(shorthandNumbers('104-02'));           // 104, 105, ... 202
+console.log(shorthandNumbers('545, 64:11'));       // 545, 564, 565, .. 611
+console.log(shorthandNumbers('1:5:2'));            // 1, 2, 3, 4, 5, 6, ... 12
