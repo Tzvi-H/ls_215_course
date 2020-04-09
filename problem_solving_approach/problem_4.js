49eccd5378f2df8af49ba21c4432dfb8c61ecf15
Tzvi-H <16guitar@gmail.com>
2020-04-09 13:29:39 -0500

    solution problem 4

--- problem_solving_approach/problem_4.js
+++ problem_solving_approach/problem_4.js
@@ -3,7 +3,11 @@ function splitIntoNums(string) {
 }
 
 function includesRange(numString) {
-  return /[:\-]|\.{2}/.test(numString);
+  return /-|:|\.{2}/.test(numString);
+}
+
+function isEmpty(array) {
+  return array.length === 0;
 }
 
 function lastElement(array) {
@@ -22,11 +26,14 @@ function endsWith(num, endingString) {
 }
 
 function splitIntoRange(string) {
-  return string.split(/[:\-]|\.{2}/);
+  return string.split(/-|:|\.{2}/);
 }
 
 function incrementNumStringByOne(numString) {
   let nextNum = Number(numString) + 1;
+  if (nextNum === 100) {
+    nextNum = 0;
+  }
   return String(nextNum);
 }
 
@@ -37,9 +44,16 @@ function convertShortHandNums(stringOfNums) {
     if (includesRange(numString)) {
       let range = splitIntoRange(numString);
       for (let index = 0; index < range.length - 1; index += 1) {
-        let lastNum = lastElement(results) || Number(range[index]);
-        let start = nextValue(lastNum, range[index]);
-        let end = nextValue(Number(start), range[index + 1]);        
+        let start = range[index];
+        let end = range[index + 1];
+        if (isEmpty(results)) {
+          results.push(Number(start));
+          start = incrementNumStringByOne(start);
+        }
+        let lastNum = lastElement(results);
+        if (endsWith(lastNum, start)) {
+          start = incrementNumStringByOne(start);
+        }
         while (true) {
           let num = nextValue(lastNum, start);
           results.push(num);
@@ -51,17 +65,21 @@ function convertShortHandNums(stringOfNums) {
         }
       }
     } else {
-      let lastNum = lastElement(results) || Number(numString);
-      let num = nextValue(lastNum, numString);
-      results.push(num);
+      if (isEmpty(results)) {
+        results.push(Number(numString));
+      } else {
+        let lastNum = lastElement(results);
+        let num = nextValue(lastNum, numString);
+        results.push(num);
+      }
     }
   })
   return results;
 }
 
-console.log(convertShortHandNums('1, 3, 7, 2, 4, 1')); // --> 1, 3, 7, 12, 14, 21
-console.log(convertShortHandNums('1-3, 1-2'));      // --> 1, 2, 3, 11, 12
-console.log(convertShortHandNums('104-2'));         // --> 104, 105, ... 112
-console.log(convertShortHandNums('104-02'));        // --> 104, 105, ... 202
-console.log(convertShortHandNums('545, 64:11'));    // --> 545, 564, 565, .. 611
-console.log(convertShortHandNums('1:5:2'));         // --> 1, 2, 3, 4, 5, 6, ... 12
+console.log(convertShortHandNums('1, 3, 7, 2, 4, 1')); // 1, 3, 7, 12, 14, 21
+console.log(convertShortHandNums('1-3, 1-2'));         // 1, 2, 3, 11, 12
+console.log(convertShortHandNums('104-2'));            // 104, 105, ... 112
+console.log(convertShortHandNums('104-02'));           // 104, 105, ... 202
+console.log(convertShortHandNums('545, 64:11'));       // 545, 564, 565, .. 611
+console.log(convertShortHandNums('1:5:2'));            // 1, 2, 3, 4, 5, 6, ... 12
